6fabf87d96811f4dd7d6b3e873006ce92f57bfd0
Andrii Tugai <andrii.tugai@gmail.com>
2020-02-01 07:40:03 +0200

    My home work for week_01

--- Week_1/hw2.py
+++ Week_1/hw2.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 arr = [
     {"name": "Jack", "age": 20},
     {"name": "Mike", "age": 22},
@@ -9,31 +7,46 @@ arr = [
 ]
 
 
-def filter_by_age(members_list, age_limit):
-    ''' Return list of members whose age gretaer or equal to age_limit'''
+def main():
 
-    return [member for member in members_list if member["age"] >= age_limit]
+    # we need to create NEW (filtered on "age" >= 20) list
+    arr_filtered = [user for user in arr if user["age"] >= 20]
 
+    # print it
+    divider = '+ {0:-^20} + {0:-^8} +'.format('')
+    header = '| {0:^20} | {1:^8} |'.format("Name", "Age")
 
-def sort_and_modify(members_list, field_to_reverse):
-    ''' Return list of members sorted by age (in descending order). 
-    Add new field "reversed" which contains reversed value of ["field_to_reverse"]'''
+    print('arr, filtered, min age = 20:')
+    print(divider)
+    print(header)
+    print(divider)
 
-    new_list = sorted(members_list, key=lambda item: item["age"], reverse=True)
-    for member in new_list:
-        member["reversed"] = member[field_to_reverse][::-1]
-    return new_list
+    for user in arr_filtered:
+        print('| {name:<20} | {age:^8} |'.format(**user))
+
+    print(divider)
+    print()
+    # --------------
+    # We don't need to create new list,
+    # just print sorted arr with additional field
+
+    divider = '+ {0:-^20} + {0:-^8} + {0:-^20} +'.format('')
+    header = '| {0:^20} | {1:^8} | {2:^20} |' \
+        .format("Name", "Age", "Name(reversed)")
+
+    print('arr, sorted and modified:')
+    print(divider)
+    print(header)
+    print(divider)
+
+    for user in sorted(arr, key=lambda user: user["age"], reverse=True):
+        print(
+            '| {name:<20} | {age:>8} | {:>20} |'
+            .format(user["name"][::-1], **user)
+        )
+
+    print(divider)
 
 
 if __name__ == '__main__':
-
-    print('arr, init values:')
-    pprint(arr)
-    # --------------
-    print('='*60)
-    print('arr, filtered, min age = 20:')
-    pprint(filter_by_age(arr, 20))
-    # --------------
-    print('='*60)
-    print('arr, sorted and modified:')
-    pprint(sort_and_modify(arr, "name"))
+    main()
--- Week_1/hw3.py
+++ Week_1/hw3.py
@@ -3,19 +3,21 @@ import sys
 
 
 def receipts_input(input_set_name: str) -> set:
-    ''' Just a shell to improve communication with user
+    """ Just a shell to improve communication with user
     and let him a chance to avoid empty input.
     input_set_name is needed to communicate with user.
     input_set_name should be a noun describing content of the set in plural form.
-    '''
+    """
+
     def user_input_(input_set_name: str) -> set:
-        ''' returns set from user input.'''
+        """ returns set from user input."""
 
         input_set_ = set()
         while True:
             print(f"There are {len(input_set_)} {input_set_name} in the set")
             input_item = input(
-                f"Enter another {input_set_name[:-1]} (empty input for exit): ")
+                f"Enter another {input_set_name[:-1]} (empty input for exit): "
+            )
             if not input_item:
                 break
 
@@ -28,10 +30,13 @@ def receipts_input(input_set_name: str) -> set:
         input_set = user_input_(input_set_name)
 
         if not input_set:
-            print(f"""
+            print(
+                f"""
             Your list of {input_set_name} is empty.
-            No receipts can be generated.""")
-            if not input("Are you sure to quit? (YES/no)"):
+            No receipts can be generated."""
+            )
+            doit_again = input("Are you sure to quit? (YES/no)")
+            if not doit_again or doit_again.lower()[0] == "y":
                 sys.exit(0)
         else:
             break
@@ -42,7 +47,7 @@ def receipts_input(input_set_name: str) -> set:
 def create_receipt(ingredients, ways, units):
     # number ingredients in the receipt:
     random.seed(300)
-    num_i = random.randrange(1, len(ingredients)+1)
+    num_i = random.randrange(1, len(ingredients) + 1)
 
     random.shuffle(ingredients)
     receipt_ingredients = ingredients[:num_i]
@@ -54,7 +59,7 @@ def create_receipt(ingredients, ways, units):
         item = {
             "ingredient": ingredient,
             "unit": random.choice(units),
-            "qty": random.randint(1, 100)
+            "qty": random.randint(1, 100),
         }
         receipt.append(item)
     way_to_prepare = random.choice(ways)
@@ -63,27 +68,27 @@ def create_receipt(ingredients, ways, units):
 
 
 def print_receipt(receipt, way_to_prepare):
-    divider = '='*52
+    divider = "=" * 52
     print("RECEIPT")
     print(divider)
     for item in receipt:
-        print('{ingredient:<30} {unit:^12} {qty:>8}'.format(**item))
+        print("{ingredient:<30} {unit:^12} {qty:>8}".format(**item))
 
     print(divider)
-    print(f'Way to prepare: {way_to_prepare}')
+    print(f"Way to prepare: {way_to_prepare}")
 
 
 def main():
-    print('INGREDIENTS:')
-    print('='*20)
+    print("INGREDIENTS:")
+    print("=" * 20)
     ingredients = list(receipts_input("ingredients"))
     print()
-    print('MEASUREMENT UNITS:')
-    print('='*20)
+    print("MEASUREMENT UNITS:")
+    print("=" * 20)
     units = list(receipts_input("units"))
     print()
-    print('PREPARATION WAYS:')
-    print('='*20)
+    print("PREPARATION WAYS:")
+    print("=" * 20)
     ways = list(receipts_input("preparation_ways"))
 
     print()
@@ -91,5 +96,5 @@ def main():
     print_receipt(*create_receipt(ingredients, ways, units))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
