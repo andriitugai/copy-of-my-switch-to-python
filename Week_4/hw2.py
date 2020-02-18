337a4e4a196ce01ff1656d084c07a07c481786e3
Andrii Tugai <andrii.tugai@gmail.com>
2020-02-18 13:09:03 +0200

    Week_04: Added hw2.py

---
+++ Week_4/hw2.py
@@ -0,0 +1,39 @@
+import itertools
+class NumberedLinesFileIterator(object):
+
+    def __init__(self, file):
+        self._filename = file        
+
+    def __iter__(self):
+        cnt = itertools.count()
+        with open(self._filename,"r") as input_file:
+            while True:
+                line = input_file.readline()
+                if not line:
+                    break
+                
+                yield f"{next(cnt):<5}|   {line}"        
+
+    def __enter__(self):
+        return self.__iter__()
+
+    def __exit__(self, *args):
+        pass
+
+def main():
+    print("Execution in common way:")
+    iterable = NumberedLinesFileIterator(file="file.txt")
+    for line in iterable:
+        print(line, end="")
+
+    print()
+
+    print("Execution using context manager:")
+    with NumberedLinesFileIterator(file="file.txt") as iterable:
+        for line in iterable:
+            print(line, end="")
+
+
+if __name__ == '__main__':
+    main()
+
